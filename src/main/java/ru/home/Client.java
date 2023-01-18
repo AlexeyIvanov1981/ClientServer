b6635b3c43b70fa6289eb28005a7b61aa548f7df
AlexeyIvanov1981 <lelikus81@yandex.ru>
2023-01-18 13:48:51 +0300

    Added logic in ServerClass

--- src/main/java/ru/home/Client.java
+++ src/main/java/ru/home/Client.java
@@ -1,4 +1,16 @@
 package ru.home;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Client {
+    public static void main(String[] args) {
+        try(ServerSocket socket = new ServerSocket(ServerConfig.PORT)
+        ){
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/main/java/ru/home/Server.java
+++ src/main/java/ru/home/Server.java
@@ -1,4 +1,25 @@
 package ru.home;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+    public static void main(String[] args) {
+        while (true) {
+            try (ServerSocket serverSocket = new ServerSocket(ServerConfig.PORT);) {
+                try (Socket clientSocket = serverSocket.accept();
+                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
+                ) {
+                    //TODO
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 }
